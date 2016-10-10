6d190ff486311ddcd1eafceef59c1c9207996b93
S.S <ss@ss.com>
2016-10-11 04:36:00 +0900

    package structure

---
+++ temp/subpkg/cmd/subpkg/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/sky0621/study-go/temp/subpkg"
+
+func main() {
+	subpkg.DoSomething("test")
+}
---
+++ temp/subpkg/subpkg.go
@@ -0,0 +1,7 @@
+package subpkg
+
+import "fmt"
+
+func DoSomething(s string) {
+	fmt.Println(s)
+}
